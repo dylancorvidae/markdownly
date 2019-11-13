ffb01e5a728040a7c7b23c3149ff7252920d2396
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-11-13 15:34:11 -0800

    tabs

--- src/containers/Tabs.js
+++ src/containers/Tabs.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TabDeck from '../components/list/TabDeck';
 
-const List = ({   }) => {
-  
+const List = ({ tabs }) => {
+  return (
+    <TabDeck tabs={tabs} />
+  )
+}
+
+List.propTypes = {
+  tabs: PropTypes.array.isRequired
 }
