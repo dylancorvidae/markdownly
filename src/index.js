860ec574b0bb134eb160780af7347377863a17f3
Pat Haynes <pathaynes32@gmail.com>
2019-11-14 10:28:18 -0800

    fixed index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import App from './components/App';
 import 'normalize.css';
+import store from './store';
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
+
