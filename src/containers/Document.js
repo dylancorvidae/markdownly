9b9862a90d1f4da38f74f22a29072d6ae5ecbfab
Dylan Corvidae <dylancorvidae@gmail.com>
2019-11-15 14:50:49 -0800

    refactored Tabs.js using hooks

--- src/containers/Document.js
+++ src/containers/Document.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import Preview from '../components/markdown/Preview';
--- src/containers/Tabs.js
+++ src/containers/Tabs.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import TabDeck from '../components/tab/TabDeck';
-import { connect } from 'react-redux';
 import { getTitles, getSearchTerm } from '../selectors/documentSelectors';
+// eslint-disable-next-line no-unused-vars
 import { addTab, deleteTab, addSearch, getMatchTitle } from '../actions/documentActions';
 import Filter from '../components/filter/Filter';
 
-const Tabs = ({ handleClick, titles, handleDelete, handleChange, handleSubmit, searchTerm }) => {
+export default function Tabs() {
+  const titles = useSelector(state => getTitles(state));
+  const searchTerm = useSelector(state => getSearchTerm(state));
+  const dispatch = useDispatch();
+  const handleChange = ({ target }) => dispatch(addSearch(target.value));
+  const handleClick = tabNumber => dispatch(addTab(tabNumber));
+  const handleDelete = deleteTab => dispatch(addTab(deleteTab));
+  const handleSubmit = getMatchTitle => dispatch(addTab(getMatchTitle));
+
   return (
     <>
       <TabDeck handleDelete={handleDelete} titles={titles} />
@@ -14,42 +23,42 @@ const Tabs = ({ handleClick, titles, handleDelete, handleChange, handleSubmit, s
       <Filter handleSubmit={() => handleSubmit(searchTerm)} handleChange={handleChange} />
     </>
   );
-};
+}
 
-const mapStateToProps = state => ({
-  titles: getTitles(state),
-  searchTerm: getSearchTerm(state)
-});
+// const mapStateToProps = state => ({
+//   titles: getTitles(state),
+//   searchTerm: getSearchTerm(state)
+// });
 
-const mapDispatchToProps = dispatch => ({
-  handleClick(tabNumber) {
-    dispatch(addTab(tabNumber));
-  },
-  handleDelete(title) {
-    dispatch(deleteTab(title));
-  },
-  handleSubmit(searchTerm) {
-    event.preventDefault();
-    dispatch(getMatchTitle(searchTerm));
-  },
-  handleChange({ target }) {
-    event.preventDefault();
-    dispatch(addSearch(target.value));
-  }
-});
+// const mapDispatchToProps = dispatch => ({
+//   handleClick(tabNumber) {
+//     dispatch(addTab(tabNumber));
+//   },
+//   handleDelete(title) {
+//     dispatch(deleteTab(title));
+//   },
+//   handleSubmit(searchTerm) {
+//     event.preventDefault();
+//     dispatch(getMatchTitle(searchTerm));
+//   },
+//   handleChange({ target }) {
+//     event.preventDefault();
+//     dispatch(addSearch(target.value));
+//   }
+// });
 
-Tabs.propTypes = {
-  titles: PropTypes.array.isRequired,
-  handleClick: PropTypes.func.isRequired,
-  handleDelete: PropTypes.func.isRequired,
-  handleChange: PropTypes.func.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  searchTerm: PropTypes.string
-};
+// Tabs.propTypes = {
+//   titles: PropTypes.array.isRequired,
+//   handleClick: PropTypes.func.isRequired,
+//   handleDelete: PropTypes.func.isRequired,
+//   handleChange: PropTypes.func.isRequired,
+//   handleSubmit: PropTypes.func.isRequired,
+//   searchTerm: PropTypes.string
+// };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Tabs);
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(Tabs);
 
 
